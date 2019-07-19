b3fe519c3a8318cb58cc71d653a93893abb1c6d5
Andrew Bay <daddnbay@yahoo.com>
2019-07-19 18:20:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,14 @@ class Song
   
   attr_accessor :name, :artist
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
+  
+  def self
   
   def self.new_by_filename(filename)
      artist_name, song_name, extra = filename.split(" - ")
