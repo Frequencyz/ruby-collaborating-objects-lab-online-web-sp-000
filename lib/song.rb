154e8ed59029cf36ae80f892dd17d4483346696e
Andrew Bay <daddnbay@yahoo.com>
2019-07-19 18:23:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,13 +7,13 @@ class Song
   end
   
   def self.new_by_filename(filename)
-        song_name = filename.split(" - ")[1]
-        artist_name = filename.split(" - ")[0]
-        artist = Artist.find_or_create_by_name(artist_name)
-        new_instance = self.new(song_name)
-        artist.add_song(new_instance)
-        new_instance.artist = artist
-        new_instance 
-    end
+      song_name = filename.split(" - ")[1]
+      artist_name = filename.split(" - ")[0]
+      artist = Artist.find_or_create_by_name(artist_name)
+      new_instance = self.new(song_name)
+      artist.add_song(new_instance)
+      new_instance.artist = artist
+      new_instance 
+  end
   
 end
